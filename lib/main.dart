131eba4a26aed2ba22763289b63305b05be8993f
Philip Yu <pyu3@mail.sfsu.edu>
2019-08-03 22:14:32 -0700

    Progress story based on User choice and restart storyline when User reaches end of story

--- lib/main.dart
+++ lib/main.dart
@@ -51,11 +51,13 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    storyBrain.nextStory(1);
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   color: Colors.red,
                   child: Text(
-                    storyBrain.getChoice1();
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -72,11 +74,13 @@ class _StoryPageState extends State<StoryPage> {
                 child: FlatButton(
                   onPressed: () {
                     //Choice 2 made by user.
-                    storyBrain.nextStory(2);
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
                   },
                   color: Colors.blue,
                   child: Text(
-                    storyBrain.getChoice2();
+                    storyBrain.getChoice2(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -90,7 +94,5 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -34,31 +34,50 @@ class StoryBrain {
   ];
   int _storyNumber = 0;
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
   String getStory() {
-    return _storyData.first.storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData.first.choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData.first.choice2;
+    return _storyData[_storyNumber].choice2;
   }
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
   void nextStory(int choiceNumber) {
-    _storyNumber = choiceNumber;
+    switch (_storyNumber) {
+      case 0:
+        if (choiceNumber == 1) {
+          _storyNumber = 2;
+        } else {
+          _storyNumber = 1;
+        }
+
+        break;
+      case 1:
+        if (choiceNumber == 1) {
+          _storyNumber = 2;
+        } else {
+          _storyNumber = 3;
+        }
+
+        break;
+      case 2:
+        if (choiceNumber == 1) {
+          _storyNumber = 5;
+        } else {
+          _storyNumber = 4;
+        }
+
+        break;
+      default:
+        print('$_storyNumber: Story is set to 0.');
+        _storyNumber = 0;
+        break;
+    }
   }
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
 }
